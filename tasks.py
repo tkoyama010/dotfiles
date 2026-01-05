7f0143ea339bbdb46a702cd7c32c6db8ceed5da9
Tetsuo Koyama <tkoyama010@gmail.com>
2026-01-05 22:45:03 +0900

    Parallelize git clone operations in vim_plugins task
    - Use ThreadPoolExecutor for concurrent repository cloning
    - Set max_workers=8 for parallel execution
    - Significantly improves installation speed for 37 repositories

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@
 import contextlib
 import logging
 import platform
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
 from invoke import Context, task
@@ -40,58 +41,64 @@ def config(c: Context) -> None:
 def vim_plugins(c: Context) -> None:
     """Installs vim plugins."""
     c.run("mkdir -p ~/.vim/pack/mypackage/start/")
-    with c.cd("~/.vim/pack/mypackage/start/"):
-        repos = [
-            "git@github.com:EdenEast/nightfox.nvim",
-            "git@github.com:PProvost/vim-ps1",
-            "git@github.com:Shougo/dein.vim",
-            "git@github.com:Shougo/deoplete.nvim",
-            "git@github.com:Shougo/neocomplcache",
-            "git@github.com:Shougo/neosnippet",
-            "git@github.com:Shougo/neosnippet-snippets",
-            "git@github.com:Shougo/unite.vim",
-            "git@github.com:Shougo/vimfiler.vim",
-            "git@github.com:Shougo/vimshell",
-            "git@github.com:Xuyuanp/nerdtree-git-plugin",
-            "git@github.com:Yggdroot/indentLine",
-            "git@github.com:airblade/vim-gitgutter",
-            "git@github.com:ctrlpvim/ctrlp.vim",
-            "git@github.com:dracula/vim",
-            "git@github.com:flazz/vim-colorschemes",
-            "git@github.com:github/copilot.vim",
-            "git@github.com:itchyny/lightline.vim",
-            "git@github.com:jacoborus/tender.vim",
-            "git@github.com:jpalardy/vim-slime",
-            "git@github.com:majutsushi/tagbar",
-            "git@github.com:nathanaelkane/vim-indent-guides",
-            "git@github.com:nelstrom/vim-visual-star-search",
-            "git@github.com:rickhowe/diffchar.vim",
-            "git@github.com:sainnhe/edge",
-            "git@github.com:scrooloose/nerdtree",
-            "git@github.com:scrooloose/syntastic",
-            "git@github.com:sudar/vim-arduino-syntax",
-            "git@github.com:t9md/vim-textmanip",
-            "git@github.com:tomasr/molokai",
-            "git@github.com:tpope/vim-fugitive",
-            "git@github.com:tyru/open-browser.vim",
-            "git@github.com:ujihisa/unite-colorscheme",
-            "git@github.com:vim-jp/vimdoc-ja",
-            "git@github.com:vim-scripts/VimClojure",
-            "git@github.com:vim-scripts/vcscommand.vim",
-            "git@github.com:will133/vim-dirdiff",
-        ]
-        for repo in repos:
-            dirname = Path(repo).name.replace(".nvim", "")  # remove .nvim from dirname
-            dest_dir = Path("~/.vim/pack/mypackage/start/") / dirname
-            dest_dir = dest_dir.expanduser()
-            if dest_dir.exists():
-                if (dest_dir / ".git").exists():
-                    with contextlib.suppress(Exception):
-                        c.run(f"cd {dest_dir} && git pull && cd ..")
-                else:
-                    pass
-            else:
-                c.run(f"git clone {repo} {dest_dir}")
+    
+    repos = [
+        "git@github.com:EdenEast/nightfox.nvim",
+        "git@github.com:PProvost/vim-ps1",
+        "git@github.com:Shougo/dein.vim",
+        "git@github.com:Shougo/deoplete.nvim",
+        "git@github.com:Shougo/neocomplcache",
+        "git@github.com:Shougo/neosnippet",
+        "git@github.com:Shougo/neosnippet-snippets",
+        "git@github.com:Shougo/unite.vim",
+        "git@github.com:Shougo/vimfiler.vim",
+        "git@github.com:Shougo/vimshell",
+        "git@github.com:Xuyuanp/nerdtree-git-plugin",
+        "git@github.com:Yggdroot/indentLine",
+        "git@github.com:airblade/vim-gitgutter",
+        "git@github.com:ctrlpvim/ctrlp.vim",
+        "git@github.com:dracula/vim",
+        "git@github.com:flazz/vim-colorschemes",
+        "git@github.com:github/copilot.vim",
+        "git@github.com:itchyny/lightline.vim",
+        "git@github.com:jacoborus/tender.vim",
+        "git@github.com:jpalardy/vim-slime",
+        "git@github.com:majutsushi/tagbar",
+        "git@github.com:nathanaelkane/vim-indent-guides",
+        "git@github.com:nelstrom/vim-visual-star-search",
+        "git@github.com:rickhowe/diffchar.vim",
+        "git@github.com:sainnhe/edge",
+        "git@github.com:scrooloose/nerdtree",
+        "git@github.com:scrooloose/syntastic",
+        "git@github.com:sudar/vim-arduino-syntax",
+        "git@github.com:t9md/vim-textmanip",
+        "git@github.com:tomasr/molokai",
+        "git@github.com:tpope/vim-fugitive",
+        "git@github.com:tyru/open-browser.vim",
+        "git@github.com:ujihisa/unite-colorscheme",
+        "git@github.com:vim-jp/vimdoc-ja",
+        "git@github.com:vim-scripts/VimClojure",
+        "git@github.com:vim-scripts/vcscommand.vim",
+        "git@github.com:will133/vim-dirdiff",
+    ]
+    
+    def process_repo(repo: str) -> None:
+        dirname = Path(repo).name.replace(".nvim", "")
+        dest_dir = Path("~/.vim/pack/mypackage/start/") / dirname
+        dest_dir = dest_dir.expanduser()
+        if dest_dir.exists():
+            if (dest_dir / ".git").exists():
+                with contextlib.suppress(Exception):
+                    c.run(f"cd {dest_dir} && git pull && cd ..")
+        else:
+            c.run(f"git clone {repo} {dest_dir}")
+    
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        futures = {executor.submit(process_repo, repo): repo for repo in repos}
+        for future in as_completed(futures):
+            repo = futures[future]
+            with contextlib.suppress(Exception):
+                future.result()
 
 
 @task
