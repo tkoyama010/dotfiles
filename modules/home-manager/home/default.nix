6df449aa4992a13688a8733b372f3f4e302ab2b8
Tetsuo Koyama <tkoyama010@gmail.com>
2026-08-11 02:35:16 +0900

    feat: add aws and terraform to skillicons and nix packages

--- modules/home-manager/home/default.nix
+++ modules/home-manager/home/default.nix
@@ -1,8 +1,12 @@
-{profile, ...}: {
+{profile, pkgs, ...}: {
   home = {
     username = profile.username;
     homeDirectory = profile.homeDirectory;
     stateVersion = "24.05";
+    packages = with pkgs; [
+      awscli2
+      terraform
+    ];
   };
 
   programs.home-manager.enable = true;
