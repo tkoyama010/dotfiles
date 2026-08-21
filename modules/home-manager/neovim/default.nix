028c6f47bd2e54875c385bd8f0283834ab8eedcf
Your Name <tkoyama010@gmail.com>
2026-08-22 06:29:40 +0900

    feat(neovim): set edge colorscheme for Neovim
    Add the  Vim plugin to the Neovim runtime pack and introduce an
     that sources , so the  declaration
    already present in the shared vimrc is applied under Neovim as well.

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -1,4 +1,9 @@
 {pkgs, ...}: {
-  xdg.configFile."nvim/pack/hm/start/markdown-preview-nvim".source =
-    pkgs.vimPlugins.markdown-preview-nvim;
+  xdg.configFile = {
+    "nvim/init.vim".source = ../../../nvim/init.vim;
+    "nvim/pack/hm/start/markdown-preview-nvim".source =
+      pkgs.vimPlugins.markdown-preview-nvim;
+    "nvim/pack/hm/start/edge".source =
+      pkgs.vimPlugins.edge;
+  };
 }
