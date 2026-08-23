2510586490414ef475b9d869caefda47243ef818
Tetsuo Koyama <tkoyama010@gmail.com>
2026-08-23 13:58:13 +0900

    feat(pi): add /exit command as alias for /quit (#189)
    * feat(pi): add caveman-auto extension to always activate caveman mode
    
    * feat(pi): add /exit command as alias for /quit
    
    pi ships /quit but not /exit. Register /exit via extension calling
    ctx.shutdown() for CLI muscle-memory consistency.

--- modules/home-manager/pi/default.nix
+++ modules/home-manager/pi/default.nix
@@ -4,5 +4,6 @@
     ".pi/agent/themes/tkoyama010.json".source = ./tkoyama010-theme.json;
     ".pi/agent/settings.json".source = ./settings.json;
     ".pi/agent/extensions/caveman-auto.ts".source = ./caveman-auto.ts;
+    ".pi/agent/extensions/exit-command.ts".source = ./exit-command.ts;
   };
 }
