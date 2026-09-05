d8bbe62314f91bebb1609b54fc56bbbc16de2ea2
Your Name <tkoyama010@gmail.com>
2026-09-05 09:33:03 +0900

    feat: add istats for macOS temperature sensors via nix

--- modules/home-manager/home/default.nix
+++ modules/home-manager/home/default.nix
@@ -1,19 +1,24 @@
 {
   profile,
   pkgs,
+  system,
   ...
-}: {
+}: let
+  isDarwin = builtins.match ".*-darwin" system != null;
+in {
   home = {
     username = profile.username;
     homeDirectory = profile.homeDirectory;
     stateVersion = "24.05";
-    packages = with pkgs; [
-      awscli2
-      gh
-      terraform
-      rtk
-      vim
-    ];
+    packages = with pkgs;
+      [
+        awscli2
+        gh
+        terraform
+        rtk
+        vim
+      ]
+      ++ pkgs.lib.optionals isDarwin [istats];
   };
 
   programs.home-manager.enable = true;
---
+++ pkgs/istats/default.nix
@@ -0,0 +1,61 @@
+{
+  pkgs,
+  lib,
+}: let
+  version = "1.6.2";
+  commit = "15813fe211e31425f7d0ad0e1018ef3102d16664";
+in
+  pkgs.stdenv.mkDerivation {
+    pname = "istats";
+    inherit version;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Chris911";
+      repo = "iStats";
+      rev = commit;
+      sha256 = "sha256-00arr5w2i8lin0ls6d7511dl373hv78ak59ba31sihnii41z21jd";
+    };
+
+    nativeBuildInputs = [pkgs.ruby];
+    buildInputs = lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.darwin.apple_sdk.frameworks.IOKit
+      pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+    ];
+
+    NIX_LDFLAGS = lib.optionalString pkgs.stdenv.isDarwin "-framework IOKit -framework CoreFoundation";
+
+    buildPhase = ''
+      cd ext/osx_stats
+      ruby extconf.rb
+      make
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/lib/ruby
+
+      # Install Ruby library files
+      cp -r lib/* $out/lib/ruby/
+
+      # Install the native extension
+      mkdir -p $out/lib/ruby/osx_stats
+      cp ext/osx_stats/osx_stats.bundle $out/lib/ruby/osx_stats/
+
+      # Install the bin script with correct load path
+      cp bin/istats $out/bin/istats
+      patchShebangs $out/bin/istats
+
+      # Wrap with RUBYLIB pointing to our lib dir
+      mv $out/bin/istats $out/bin/.istats-unwrapped
+      makeWrapper $out/bin/.istats-unwrapped $out/bin/istats \
+        --set RUBYLIB "$out/lib/ruby" \
+        --prefix PATH : "${lib.makeBinPath [pkgs.ruby]}"
+    '';
+
+    meta = {
+      description = "Command-line tool to grab CPU temperature, fan speeds and battery information on macOS";
+      homepage = "https://github.com/Chris911/iStats";
+      license = lib.licenses.mit;
+      platforms = ["aarch64-darwin" "x86_64-darwin"];
+      mainProgram = "istats";
+    };
+  }
