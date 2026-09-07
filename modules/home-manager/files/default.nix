60fdaaa17f1053cb7065eade4f00176223205d8c
Your Name <tkoyama010@gmail.com>
2026-09-07 21:19:14 +0900

    feat(nvim): migrate from vimscript to lua config
    - Replace init.vim (sourced ~/.vimrc) with native init.lua
    - Convert all vimrc settings to vim.opt / vim.g / vim.cmd Lua equivalents
    - Remove standalone vimrc file — Neovim is now fully self-contained
    - Drop t_Co=256 (Neovim always supports 256 colors)

--- modules/home-manager/files/default.nix
+++ modules/home-manager/files/default.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
   home.file = {
-    ".vimrc".source = ../../../vimrc;
     ".profile".source = ../../../.profile;
     ".config/nix/nix.conf".text = "extra-experimental-features = nix-command flakes\n";
   };
