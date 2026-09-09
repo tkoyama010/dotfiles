daddb8a38678f0f21d0944be07b5c171bb60a12a
Tetsuo Koyama <tkoyama010@gmail.com>
2026-09-10 06:04:25 +0900

    chore: remove tkoyama010 from theme name, fix remote-pi agent_name

--- modules/home-manager/pi/default.nix
+++ modules/home-manager/pi/default.nix
@@ -1,6 +1,6 @@
 {...}: {
   home.file = {
-    ".pi/agent/themes/tkoyama010.json".source = ./tkoyama010-theme.json;
+    ".pi/agent/themes/default.json".source = ./theme.json;
     ".pi/agent/settings.json" = {
       source = ./settings.json;
       force = true;
