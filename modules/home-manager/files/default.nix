c12f0466198efbb0038b0be706d4257bb348e3b3
Tetsuo Koyama <tkoyama010@gmail.com>
2026-09-11 08:37:01 +0900

    feat(home-manager): install .envrc via nix

--- modules/home-manager/files/default.nix
+++ modules/home-manager/files/default.nix
@@ -3,6 +3,7 @@
   home.file = {
     ".vimrc".source = ../../../vimrc;
     ".profile".source = ../../../.profile;
+    ".envrc".source = ../../../.envrc;
     ".config/nix/nix.conf".text = "extra-experimental-features = nix-command flakes\n";
   };
 }
