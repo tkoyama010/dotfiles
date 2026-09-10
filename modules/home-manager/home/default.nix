b895685a606347484bbf0de12703c8462ae0be52
Tetsuo Koyama <tkoyama010@gmail.com>
2026-09-11 08:42:10 +0900

    refactor(pi): move PI_MODEL to home.sessionVariables for all-shell support

--- modules/home-manager/home/default.nix
+++ modules/home-manager/home/default.nix
@@ -22,4 +22,9 @@ in {
   };
 
   programs.home-manager.enable = true;
+
+  home.sessionVariables = {
+    PI_MODEL = "qwen3.6-plus";
+    PI_CODING_AGENT = "true";
+  };
 }
--- modules/home-manager/shell/default.nix
+++ modules/home-manager/shell/default.nix
@@ -14,8 +14,6 @@
     sessionVariables = {
       EDITOR = "vim";
       CAVEMAN_DEFAULT_MODE = "full";
-      PI_MODEL = "qwen3.6-plus";
-      PI_CODING_AGENT = "true";
     };
     # Volta stays in .zshenv rather than .zshrc on purpose. path_helper in
     # /etc/zprofile demotes anything .zshenv prepends, which currently leaves
